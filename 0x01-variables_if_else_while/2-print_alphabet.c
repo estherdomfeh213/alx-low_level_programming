9240af205301b68cf00dc1e501bf6e8fba597c1c
estherdomfeh213 <domfehesther2020@gmail.com>
2023-09-10 21:41:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function entry point of this task
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+
+}

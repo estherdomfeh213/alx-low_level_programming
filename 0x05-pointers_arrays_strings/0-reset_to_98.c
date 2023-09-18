5c39624cea92a7eb18d75492676ce19df58efaa0
estherdomfeh213 <domfehesther2020@gmail.com>
2023-09-18 12:31:18 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * rest_to_98 - a function name that take a parameter
+  * @n: a function parameter
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the readme for this pointer , array and string project

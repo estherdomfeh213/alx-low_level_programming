853473c9fc9ad80829b00f956f9613cca9491836
estherdomfeh213 <domfehesther2020@gmail.com>
2023-09-18 16:02:05 -0700

    Rev_print_update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - the function name
+ * @str: the function parameter
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}

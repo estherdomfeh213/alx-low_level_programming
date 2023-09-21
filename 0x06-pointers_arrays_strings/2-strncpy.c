b89210de2324b315f05d5bf41ce4a3bd6e8aa5ac
estherdomfeh213 <domfehesther2020@gmail.com>
2023-09-21 00:57:28 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - a function name to copy a string
+ * @dest: function parameter 1
+ * @src: function parameter 2
+ * @n: function parameter 3
+ * Retrun: always dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}

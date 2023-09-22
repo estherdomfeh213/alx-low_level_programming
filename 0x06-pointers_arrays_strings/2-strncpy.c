09f6a945ca64700382ad48b0a3120c41c600ccc1
estherdomfeh213 <domfehesther2020@gmail.com>
2023-09-22 15:23:10 -0700

    updated_2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * @dest: function parameter 1
  * @src: function parameter 2
  * @n: function parameter 3
- * Retrun: always dest
+ * Return: always dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
